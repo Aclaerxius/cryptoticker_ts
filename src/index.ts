91e2f316dc7fe0a765db36aabe7e43ec340df630
Alexandru Carusu <41578311+Aclaerxius@users.noreply.github.com>
2024-05-08 18:12:12 +0300

    Files upload

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { G, Test } from "test.ts";
+
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+const user = "Jane User";
+const test = new Test();
+console.log(test.t, G);
+console.log(greeter(user));
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export const G = 100;
+
+export class Test {
+  t: number = 50;
+}
